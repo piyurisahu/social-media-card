dfaec084f410b2f8080f7bdab71fb8e8d881f5ee
Admin <admin@Admins-MacBook-Pro.local>
2018-07-23 15:25:11 +0530

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    // Everything flows from this file
+    entry: "./app/index.js",
+    // Once Webpack is done, it assembles the file and deposits it
+    // in the current directory in the build subdirectory, with a name of build.js
+    output: {
+        path: __dirname + "/build",
+        filename: "build.js"
+    },
+    module: {
+        rules: [
+            {
+                // Watch all files that end in .js
+                test: /\.jsx?$/,
+                // Unless it's in the node_modules directory
+                exclude: /node_modules/,
+                // And run it through the babel loader
+                loader: 'babel-loader'
+            }
+        ]
+    },
+
+    devServer: {
+        inline: true,
+        port: 8009
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: './app/index.html'
+    })]
+}
